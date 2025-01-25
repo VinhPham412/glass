e86883b5c6be9c84da447053f8bba020cccc8d2f
Hieubkav <tranmanhhieu10@gmail.com>
2025-01-26 01:24:01 +0700

    hoàn thiện gợi ý kính

--- resources/views/main/try_on.blade.php
+++ resources/views/main/try_on.blade.php
@@ -69,7 +69,7 @@
                     glassesHeight
                 );
 
-                // // vẽ ra đường centerX và centerY
+                //  vẽ ra đường centerX và centerY
                 // canvasCtx.beginPath();
                 // canvasCtx.arc(centerX, centerY, 5, 0, 2 * Math.PI);
                 // canvasCtx.fillStyle = 'red';
@@ -119,6 +119,102 @@
                 //     canvasCtx.fillText(i, landmark.x * canvasElement.width, landmark.y * canvasElement.height);
                 // }
 
+                // Tính tỉ lệ khuon mặt dựa trên cac diem tren khuon mat
+                const canvasWidth = canvasElement.width;
+                const canvasHeight = canvasElement.height;
+                // Lấy các điểm quan trọng trên khuôn mặt
+                const chin = landmarks[152]; // Điểm cằm
+                const forehead = landmarks[10]; // Điểm trán
+                const leftCheek = landmarks[234]; // Gò má trái
+                const rightCheek = landmarks[454]; // Gò má phải
+                const leftTemple = landmarks[127]; // Thái dương trái
+                const rightTemple = landmarks[356]; // Thái dương phải
+                // Xương quai hàm trái
+                const leftJaw = landmarks[58];
+                // Xương quai hàm phải
+                const rightJaw = landmarks[288];
+
+                // Tính chiều dài và chiều rộng khuôn mặt
+                const faceWidth = Math.sqrt(
+                    Math.pow(rightTemple.x - leftTemple.x, 2) +
+                    Math.pow(rightTemple.y - leftTemple.y, 2)
+                ) * canvasWidth;
+
+                const faceHeight = Math.sqrt(
+                    Math.pow(forehead.x - chin.x, 2) +
+                    Math.pow(forehead.y - chin.y, 2)
+                ) * canvasHeight;
+
+                // Tính khoảng cách 2 xương hàm
+                const jawDistance = Math.sqrt(
+                    Math.pow(rightJaw.x - leftJaw.x, 2) +
+                    Math.pow(rightJaw.y - leftJaw.y, 2)
+                ) * canvasWidth;
+
+                // Tính khoảng cách 2 gò má
+                const cheekDistance = Math.sqrt(
+                    Math.pow(rightCheek.x - leftCheek.x, 2) +
+                    Math.pow(rightCheek.y - leftCheek.y, 2)
+                ) * canvasWidth;
+
+                // Tính khoảng cách 2 thái dương
+                const templeDistance = Math.sqrt(
+                    Math.pow(rightTemple.x - leftTemple.x, 2) +
+                    Math.pow(rightTemple.y - leftTemple.y, 2)
+                ) * canvasWidth;
+
+                // Tính tỷ lệ chiều dài và chiều rộng
+                const ratio = faceHeight / faceWidth;
+                // Tính tỷ lệ chiều rộng quai hàm và chiều rộng khuôn mặt
+                const jawRatio = jawDistance / faceWidth;
+                // Tính tỷ lệ chiều rộng gò má và chiều rộng khuôn mặt
+                const cheekRatio = cheekDistance / faceWidth;
+                // Tính tỷ lệ chiều rộng thái dương và chiều rộng khuôn mặt
+                const templeRatio = templeDistance / faceWidth;
+
+                // Vẽ các điểm chính để kiểm tra, có ghi tên điểm
+                // [chin, forehead, leftCheek, rightCheek, leftTemple, rightTemple, leftJaw, rightJaw].forEach((point, index) => {
+                //     canvasCtx.beginPath();
+                //     canvasCtx.arc(point.x * canvasWidth, point.y * canvasHeight, 5, 0, 2 * Math.PI);
+                //     canvasCtx.fillStyle = 'red';
+                //     canvasCtx.fill();
+                //     canvasCtx.stroke();
+                //     canvasCtx.fillText(['Chin', 'Forehead', 'Left Cheek', 'Right Cheek', 'Left Temple', 'Right Temple', 'Left Jaw', 'Right Jaw'][index], point.x * canvasWidth, point.y * canvasHeight);
+                //     canvasCtx.stroke();
+                //     canvasCtx.closePath();
+                // });
+
+                // Xác định hình dạng khuôn mặt dựa trên tỷ lệ ratio, jawRatio, cheekRatio, templeRatio
+                let faceShape = '';
+                if (ratio > 1.3) {
+                    faceShape = 'Mặt Oval => Hợp với kính tròn';
+                } else if (ratio >= 1.1 && ratio <= 1.3) {
+                    if (jawRatio > 0.78) {
+                        faceShape = 'Mặt Vuông => Hợp với kính vuông';
+                    } else if (cheekRatio > 0.8) {
+                        faceShape = 'Mặt Tròn => Hợp với kính tròn';
+                    } else {
+                        faceShape = 'Mặt Kim Cương => Hợp với kính vuông';
+                    }
+                } else if (ratio < 1.1) {
+                    if (templeRatio > 0.8) {
+                        faceShape = 'Mặt Trái Tim => Hợp với kính tròn';
+                    } else if (jawRatio > 0.78) {
+                        faceShape = 'Mặt Chữ Nhật => Hợp với kính vuông';
+                    } else {
+                        faceShape = 'Mặt Tam Giác => Hợp với kính vuông';
+                    }
+                }
+
+                // Vẽ tên hình dạng khuôn mặt lên canvas
+                canvasCtx.font = "20px Arial";
+                canvasCtx.fillStyle = "blue";
+                canvasCtx.fillText(
+                    `Khuôn mặt: ${faceShape}`,
+                    canvasWidth / 2 - 100,
+                    30
+                );
+
 
             }
         }
@@ -147,5 +243,7 @@
     });
     camera.start();
 </script>
+
+
 </body>
 </html>
